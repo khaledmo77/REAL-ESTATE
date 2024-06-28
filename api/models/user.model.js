62d0294cadbb80a2ae3e2b6b36f5b647dadf78ca
khaled <khaled_mohamed987@yahoo.com>
2024-06-28 16:22:41 +0300

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema(
+    {
+        username :{
+            type: string,
+            required : true,
+            unique: true,
+
+
+        },
+        email :{
+            type: string,
+            required : true,
+            unique: true,
+            
+
+        },
+        password :{
+            type: string,
+            required : true,
+   
+            
+
+        }
+    }, {timestamps:true} //to record time of update user and add user// 
+);
+const User = mongoose.model('User',userSchema);
+
+
+
+export default User;
